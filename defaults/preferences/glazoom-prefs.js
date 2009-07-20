4737e3afb2b81151a1d9dd901e0ca22195ebc912
Arvid Jakobsson <arvid.jakobsson@gmail.com>
2009-07-21 00:01:03 +0200

    first commit

---
+++ defaults/preferences/glazoom-prefs.js
@@ -0,0 +1,7 @@
+pref("extensions.glazoom.maxZoomFactor", 0);
+pref("extensions.glazoom.preferTableCells", false);
+pref("extensions.glazoom.autoZoom", 100);
+pref("extensions.glazoom.buttonsStyle", "magglasses");
+pref("extensions.glazoom.firstRun.versions", "");
+
+pref("toolkit.zoomManager.applyPrefToSetting", 0);
